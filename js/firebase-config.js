4d98c3e4fe8270b8ac0a98c873669a904637a6ad
labor0leo <joy9274@naver.com>
2025-08-28 14:57:48 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,12 @@ console.log('🔥 Firebase 설정 파일 로드 시작');
 
 // Firebase 설정
 const firebaseConfig = {
-  apiKey: "AIzaSyAO-tMIvPnoFVoTWNxviAAsdi8rsNfInRg",
-  authDomain: "test-cb6b4.firebaseapp.com",
-  projectId: "test-cb6b4",
-  storageBucket: "test-cb6b4.firebasestorage.app",
-  messagingSenderId: "508605544544",
-  appId: "1:508605544544:web:bd149313777711a93d6b05"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Firestore 데이터베이스 함수들
